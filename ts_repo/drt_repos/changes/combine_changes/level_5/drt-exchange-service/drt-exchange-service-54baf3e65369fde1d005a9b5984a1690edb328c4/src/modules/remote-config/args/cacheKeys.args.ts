2606ffeac3034c7c5bc0af5c8cb3433bf25b7d59
teja78906 <tejaswaroop0507@gmail.com>
2025-07-16 17:38:30 +0530

    exchange service

--- ts_repo/drt_repos/changes/combine_changes/level_5/drt-exchange-service/drt-exchange-service-54baf3e65369fde1d005a9b5984a1690edb328c4/src/modules/remote-config/args/cacheKeys.args.ts
+++
@@ -1,7 +0,0 @@
-import { ArgsType, Field } from '@nestjs/graphql';
-
-@ArgsType()
-export class CacheKeysArgs {
-    @Field(() => [String])
-    keys: string[];
-}
--- ts_repo/drt_repos/changes/combine_changes/level_5/drt-exchange-service/drt-exchange-service-54baf3e65369fde1d005a9b5984a1690edb328c4/src/modules/tokens/models/tokens.response.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectType } from '@nestjs/graphql';
-import relayTypes from 'src/utils/relay.types';
-import { DcdtToken } from './dcdtToken.model';
-
-@ObjectType()
-export class TokensResponse extends relayTypes<DcdtToken>(DcdtToken) {}
